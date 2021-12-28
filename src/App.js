78c628a1bd429345fa0b2263c3157f195122235a
ianbrinkerhoff09 <ian.brinkerhoff@gmail.com>
2021-12-27 20:26:50 -0500

    making login form

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import logo from "./logo.svg";
+import Login from './login.jsx';
 import "./App.css";
 
 function App() {
     return (
-        // <div className="App">
-        //   <header className="App-header">
-        //     <img src={logo} className="App-logo" alt="logo" />
-        //     <p>
-        //       Edit <code>src/App.js</code> and save to reload.
-        //     </p>
-        //     <a
-        //       className="App-link"
-        //       href="https://reactjs.org"
-        //       target="_blank"
-        //       rel="noopener noreferrer"
-        //     >
-        //       Learn React
-        //     </a>
-        //   </header>
-        // </div>
-
-        <div className="App">
-            <header className="App-header">
-                    <div class="boxed">This text is enclosed in a box.</div>
-            </header>
-        </div>
+        <Login />
     );
 }
 
