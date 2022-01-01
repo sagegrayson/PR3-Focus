5ee79a27d02704fd792980eb1c63dea822c24828
ianbrinkerhoff09 <ian.brinkerhoff@gmail.com>
2022-01-01 17:51:12 -0500

    initial routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import Login from './login.jsx';
-import "./App.css";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+import { Chat } from './components/Chat';
+import { Account } from './components/Account';
 
-function App() {
-    return (
-        <Login />
-    );
+
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Router>
+                    <Routes>
+                        <Route exact path='/home' componant={Home} />
+                        <Route path='/login' componant={Login} />
+                        <Route path='/register' componant={Register} />
+                        <Route path='/chat' componant={Chat} />
+                        <Route path='/account' componant={Account} />
+                    </Routes>
+                </Router>
+
+            </React.Fragment>
+        )
+    }
 }
+
 
 export default App;
---
+++ src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Chat = () => (
+    <div>
+        <p>
+            Test
+        </p>
+    </div>
+);
