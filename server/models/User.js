8b17be0595bd3b8892cc1a0d0792985e40b6c299
Anthony Ellis <amellis245@gmail.com>
2022-01-03 12:12:43 -0500

    created User model

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [({ length }) => length >= 6, "Password should be longer."],
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
