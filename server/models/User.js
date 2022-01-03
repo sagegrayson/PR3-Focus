cc8c4677f7efd55891680949ca807e5efa747daa
Anthony Ellis <amellis245@gmail.com>
2022-01-03 13:37:12 -0500

    installed bcrypt npm package

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,7 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({
+const userSchema = new Schema({
   _id: {
     type: String,
     unique: true,
@@ -20,6 +19,21 @@ const UserSchema = new Schema({
   },
 });
 
-const User = mongoose.model("User", UserSchema);
+// set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
