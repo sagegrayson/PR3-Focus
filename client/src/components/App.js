dd03dcef1aff93a9dd34bdc5d974f36a083cfd89
James Sgarella <Jimbo8702@gmail.com>
2022-01-03 12:19:53 -0500

    login modified and signup added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,37 @@
 import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import { ContactsProvider } from "../context/ContactsProvider";
 import { ConversationsProvider } from "../context/ConversationsProvider";
 import { SocketProvider } from "../context/SocketProvider";
+import Signup from "./Signup";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const [id, setId] = useLocalStorage("id");
 
   const dashBoard = (
-    <SocketProvider id={id}>
-      <ContactsProvider>
-        <ConversationsProvider id={id}>
-          <Dashboard id={id} />
-        </ConversationsProvider>
-      </ContactsProvider>
-    </SocketProvider>
+    <ApolloProvider client={client}>
+      <SocketProvider id={id}>
+        <ContactsProvider>
+          <ConversationsProvider id={id}>
+            <Dashboard id={id} />
+          </ConversationsProvider>
+        </ContactsProvider>
+      </SocketProvider>
+      <Router>
+        <Route>
+          <Signup onIdSubmit={setId} exact path="/Signup" />
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 
   return id ? dashBoard : <Login onIdSubmit={setId} />;
