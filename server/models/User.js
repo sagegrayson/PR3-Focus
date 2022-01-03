541cefe2e37b2e3168f10d608b09f685d978ecf2
Anthony Ellis <amellis245@gmail.com>
2022-01-03 13:37:53 -0500

    Revert "installed bcrypt npm package"
    This reverts commit cc8c4677f7efd55891680949ca807e5efa747daa.

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,8 @@
-const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 
-const userSchema = new Schema({
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
   _id: {
     type: String,
     unique: true,
@@ -19,21 +20,6 @@ const userSchema = new Schema({
   },
 });
 
-// set up pre-save middleware to create password
-userSchema.pre("save", async function (next) {
-  if (this.isNew || this.isModified("password")) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
-
-  next();
-});
-
-// compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
-
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
